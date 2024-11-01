f563a9b9a9b3f2b606e4e6a9238ade8289915ba0
Alan <alannavarro07@gmail.com>
2024-11-01 16:33:40 -0600

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FormInfoComponent } from './form-info/form-info.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:InicioComponent},
+  {path:'servicio', component:ServiciosComponent},
+  {path:'form_info', component:FormInfoComponent},
+  {path:'footer', component:FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FormInfoComponent } from './form-info/form-info.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ServiciosComponent,
+    FormInfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
